476a0f08fd45403a614fc5e5aae3e553415aaa26
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2024-03-03 03:55:21 +0800

    style fix and added scroll top for projects navigation

--- src/components/tsx/back-arrow.tsx
+++ src/components/tsx/back-arrow.tsx
@@ -1,14 +1,14 @@
 import { BxArrowBack } from "../../assets/icons/tsx/BackArrow";
 import { cn } from "../../shared/cn";
 
-function BackArrow({ className }: { className?: string }) {
+function BackArrow({ className, id }: { className?: string; id: string }) {
   return (
     <a
       className={cn(
         "flex cursor-pointer items-center gap-2 text-base text-neutral-400 transition-all hover:-translate-y-0.5 hover:text-white",
         className,
       )}
-      href="/projects#projects"
+      href={`/projects#${id}`}
     >
       <BxArrowBack className="h-6 w-6 md:h-7 md:w-7" />
       Back
