71b249e074751d39e1cf39442e25bb575b66b467
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2024-03-12 01:15:12 +0800

    feat: :sparkles: added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })]
 });
